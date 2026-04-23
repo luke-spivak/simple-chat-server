a6606a510da8f182bcc85b322f0fb2029e1a9156
Luke <68616777+vizualprophet@users.noreply.github.com>
2026-04-22 23:17:16 -0400

    add listening socket setup helper

--- src/chatd.c
+++ src/chatd.c
@@ -1,7 +1,11 @@
 #include <errno.h>
 #include <limits.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 static int parse_port(const char *text, unsigned short *out_port) {
     char *end = NULL;
@@ -20,7 +24,42 @@ static int parse_port(const char *text, unsigned short *out_port) {
     return 0;
 }
 
+static int setup_listen_socket(unsigned short port) {
+    int fd;
+    int opt;
+    struct sockaddr_in addr;
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        return -1;
+    }
+
+    opt = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        close(fd);
+        return -1;
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
+
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        close(fd);
+        return -1;
+    }
+
+    if (listen(fd, SOMAXCONN) < 0) {
+        close(fd);
+        return -1;
+    }
+
+    return fd;
+}
+
 int main(int argc, char **argv) {
+    int listen_fd;
     unsigned short port;
 
     if (argc != 2) {
@@ -34,6 +73,12 @@ int main(int argc, char **argv) {
         return 2;
     }
 
-    (void)port;
+    listen_fd = setup_listen_socket(port);
+    if (listen_fd < 0) {
+        perror("Failed to initialize listening socket");
+        return 1;
+    }
+
+    close(listen_fd);
     return 0;
 }
